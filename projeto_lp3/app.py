b54885b06231e410362f7ed757739927bbfa0285
MateusKekW <mendonca.mateus@aluno.ifsp.edu.br>
2024-06-18 09:15:54 -0300

    dia pipipipópópó

--- projeto_lp3/app.py
+++ projeto_lp3/app.py
@@ -1,48 +1,24 @@
-from flask import Flask
-from validate_docbr import CPF
-from validate_docbr import CNPJ
+from flask import Flask, render_template
+from validate_docbr import CPF, CNPJ
 
-app = Flask("minha aplicação")
-
+app = Flask(__name__)
 
 @app.route("/")
 def home ():
-    return "<h1>Home Page</h1>"
+    return render_template('index.html')
 
 @app.route("/contato")
 def contato ():
-    return "<h1>Contato</h1>"
-
-@app.route("/gerar-cpf")
-def gerarCPF ():
-    cpf = CPF()
-
-    new_cpf_one = cpf.generate(True)
-
-    return ("<h2> CPF: " + new_cpf_one + " </h2>")
-
-@app.route("/gerar-cnpj")
-def gerarCNPJ():
-    cnpj = CNPJ()
-
-    new_cnpj_one = cnpj.generate(True)
-
-    return ("<h2> CNPJ: " + new_cnpj_one + " <h2>")
-
-@app.route("/servicos")
-def servicos():
-    return "<h1> Nossos Serviços <h1>"
-
-@app.route("/sobre")
-def sobre ():
-    return "<h1>Sobre</h1>"
+    return render_template('contato.html')
 
 
-# /gerar-cpf (deve devolver um cpf random)
-# /gerar-cnpj (deve devolver um cnpj random)
-# /servicos (deve devolver um título com "Nossos Serviços")
+@app.route("/produtos")
+def produtos():
+    listaProdutos = [
+        {"home": "Coca cola", "descricao": "Mata a sede e tu tbm =)"},
+        {"home": "Bolachinha", "descricao": "Mata tua fone"}
+        ]
+    return render_template('produtos.html', produtos = listaProdutos)
 
-
-
-
-app.run()
+if __name__ == "__main__":
+    app.run()
