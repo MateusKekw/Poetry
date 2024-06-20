08831e458e55a61a52e8b3c7a5a556c10187dc41
MateusKekW <mendonca.mateus@aluno.ifsp.edu.br>
2024-06-20 15:11:00 -0300

    O pai sabe tá

--- projeto_lp3/app.py
+++ projeto_lp3/app.py
@@ -15,10 +15,22 @@ def contato ():
 @app.route("/produtos")
 def produtos():
     listaProdutos = [
-        {"home": "Coca cola", "descricao": "Mata a sede e tu tbm =)"},
-        {"home": "Bolachinha", "descricao": "Mata tua fone"}
+        {"nome": "Coca cola", "descricao": "Mata a sede e tu tbm =)", "imagem": "https://p.turbosquid.com/ts-thumb/HX/VJZEKq/Kb/01/jpg/1630333961/600x600/fit_q87/aff8f4e9ab2bb3481c7e37abfdac8e9d2aae071f/01.jpg"},
+        {"nome": "Bolachinha", "descricao": "Mata tua fome", "imagem": "https://static.itdg.com.br/images/360-240/340f3563c37c9741cb962a1309793f0c/264064-original.jpg"}
         ]
     return render_template('produtos.html', produtos = listaProdutos)
+
+@app.route("/termos")
+def termos ():
+    return render_template('termos.html')
+
+@app.route("/politica")
+def politia ():
+    return render_template('politica.html')
+
+@app.route("/howto")
+def howto ():
+    return render_template('howto.html')
 
 if __name__ == "__main__":
     app.run()
